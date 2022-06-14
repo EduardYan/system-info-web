6130302def8b973336d1798442e258f7862ee75e
EduarYan <eduarygp@gmail.com>
2022-06-13 21:06:36 -0600

    Bug for DESKTOP SESSION  fixed

--- helpers/utils.py
+++ helpers/utils.py
@@ -18,6 +18,7 @@ from models.monitor import Monitor
 datetime_current = datetime.datetime.now()
 datetime_current = datetime_current.strftime('%d/%m/%Y %H:%M:%S')
 
+# in case node not is installed or get a error
 try:
     # getting node version
     node_version = subprocess.run(['node','--version'], check = True, stdout=subprocess.PIPE).stdout.decode()
@@ -28,22 +29,41 @@ except:
 
 
 
-USER_SYSTEM = System(
-    platform.architecture(),
-    platform.system(),
-    platform.version(),
-    platform.release(),
-    os.cpu_count(),
-    os.environ['LANG'],
-    os.environ['HOME'],
-    os.environ['DESKTOP_SESSION'],
-    datetime_current,
-    platform.python_version(),
-    node_version,
-    platform.win32_edition(),
-    platform.win32_ver(),
-    platform.mac_ver(),
-)
+# in case some key of the environ dictionary is error
+try:
+    USER_SYSTEM = System(
+        platform.architecture(),
+        platform.system(),
+        platform.version(),
+        platform.release(),
+        os.cpu_count(),
+        os.environ['LANG'],
+        os.environ['HOME'],
+        os.environ['DESKTOP_SESSION'],
+        datetime_current,
+        platform.python_version(),
+        node_version,
+        platform.win32_edition(),
+        platform.win32_ver(),
+        platform.mac_ver(),
+    )
+except KeyError:
+    USER_SYSTEM = System(
+        platform.architecture(),
+        platform.system(),
+        platform.version(),
+        platform.release(),
+        os.cpu_count(),
+        os.environ['LANG'],
+        os.environ['HOME'],
+        os.environ['DESKTOP_SESSION_WM'],
+        datetime_current,
+        platform.python_version(),
+        node_version,
+        platform.win32_edition(),
+        platform.win32_ver(),
+        platform.mac_ver(),
+    )
 
 
 def get_monitor() -> Monitor:
